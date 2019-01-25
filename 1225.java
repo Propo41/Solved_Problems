7db836a1cb2b759cdc4aa24de39a8b4f2b304f94
Ali Ahnaf Swapnil <46298019+Propo41@users.noreply.github.com>
2019-01-26 00:25:09 +0600

    Update 1225.java

--- 1225.java
+++ 1225.java
@@ -25,46 +25,9 @@ public class Main {
                 }
 
                 for (int j = 0; j < k; j++) {
-                    switch (temp[j]) {
-                        case 0:
-                            arr[0]++;
-                            break;
-                        case 1:
-                            arr[1]++;
-                            break;
-                        case 2:
-                            arr[2]++;
-                            break;
-
-                        case 3:
-                            arr[3]++;
-                            break;
-
-                        case 4:
-                            arr[4]++;
-                            break;
-
-                        case 5:
-                            arr[5]++;
-                            break;
-
-                        case 6:
-                            arr[6]++;
-                            break;
-
-                        case 7:
-                            arr[7]++;
-                            break;
-
-                        case 8:
-                            arr[8]++;
-                            break;
-
-                        case 9:
-                            arr[9]++;
-                            break;
-
-                    }
+                   
+                    arr[temp[j]]++;
+                    
                 }
 
             }
